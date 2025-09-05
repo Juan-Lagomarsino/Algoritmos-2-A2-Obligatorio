6a147f79f138d4018c361427bc5aa0836e57b18e
Juan <juanpe@lagomarsino.org>
2025-09-05 16:05:01 -0300

    Estructura en ejercicio2.cpp

--- ejercicio2.cpp
+++ ejercicio2.cpp
@@ -5,6 +5,55 @@
 
 using namespace std;
 
+struct ____ {
+    int cantidadaRecursos;
+    
+
+};
+
+class ___ {
+    private:
+
+    public:
+    void PUT(dominio,path,titulo,tiempo){
+
+    };
+
+    void GET(dominio,path){
+
+    };
+    
+    void REMOVE(dominio,path){
+
+    };
+    
+    void CONTAINS(dominio,path){
+
+    };
+    
+    void COUNT_DOMAIN(dominio){
+
+    };
+    
+    void LIST_DOMAIN(dominio){
+
+    };
+
+    void CLEAR_DOMAIN(dominio){
+
+    };
+
+    void SIZE(){
+
+    };
+
+    void CLEAR(){
+
+    };
+    
+};
+
+
 int main()
 {
     // TODO
