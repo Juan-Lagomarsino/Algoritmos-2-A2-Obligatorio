826925944c983ff351ee10e4eb00534ce93ee51b
Juan <juanpe@lagomarsino.org>
2025-09-10 08:58:29 -0300

    Ej2

--- ejercicio2.cpp
+++ ejercicio2.cpp
@@ -4,20 +4,42 @@
 #include <limits>
 
 using namespace std;
-
+struct nodoRecurso{
+    string dominio;
+    string path;
+    string titulo;
+    int tiempo;
+};
 //Dice que se hace con hash , y el hash debe usar una funcion de doble hash. Pero no se sobre que, no lo pense.
-struct ____ {
+struct tablaHash {
+    nodoRecurso* tabla;
+    int largo;
     int cantidadaRecursos;
-    
-
 };
 
-class ___ {
+class hashCerrado {
     private:
+    tablaHash*tH;
 
+
+    int fh(int i,string dominio){
+        return h1(dominio) + i*h2(dominio);
+    }
+
+    int h1 (string aPoner){
+        
+    }
+    int h2 (string aPoner){
+        
+    }
+
+    void PUTRecursivo(string dominio, string path, string titulo, int tiempo){
+        if(this->tH[fh(0,dominio)])
+    }
+        
     public:
     void PUT(dominio,path,titulo,tiempo){
-
+        this->PUTRecursivo(dominio,path,titulo,tiempo);
     };
 
     void GET(dominio,path){
@@ -54,10 +76,6 @@ class ___ {
     
 };
 
-int funcionHash(___){
-    //esto tiene que ser doble hash.
-
-} 
 
 int main()
 {
